3d309ce2043472a19d5195f70a3a289a8629159f
Jiaxuan Wang <jiaxuanw1@gmail.com>
2020-04-07 21:31:44 -0400

    Replace anonymous class with nested class ButtonHandler

--- src/GUI.java
+++ src/GUI.java
@@ -21,7 +21,6 @@ public class GUI {
 	private JRadioButton add, subtract, multiply, divide;
 	private ButtonGroup group;
 	private JButton calc;
-	private JTextArea calculations;
 	private Quaternion q1, q2;
 	private boolean twoInputs;
 	private String output;
@@ -32,7 +31,7 @@ public class GUI {
 
 	private void initialize() {
 		// Frame
-		frame = new JFrame();
+		frame = new JFrame("QuaternionCalc");
 		frame.setBounds(100, 100, 800, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
@@ -144,120 +143,121 @@ public class GUI {
 
 		// Calculate button
 		calc = new JButton("Calculate");
-		calc.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				// Detect whether the user has inputted two quaternions
-				try {
-					double z = Double.parseDouble(a2.getText()) + Double.parseDouble(b2.getText())
-							+ Double.parseDouble(c2.getText()) + Double.parseDouble(d2.getText());
-					twoInputs = true;
-				} catch (Exception e) {
-					twoInputs = false;
-				}
-
-				if (twoInputs) {
-					if (add.isSelected()) {
-						try {
-							double aVal1 = Double.parseDouble(a1.getText());
-							double bVal1 = Double.parseDouble(b1.getText());
-							double cVal1 = Double.parseDouble(c1.getText());
-							double dVal1 = Double.parseDouble(d1.getText());
-							q1 = new Quaternion(aVal1, bVal1, cVal1, dVal1);
-
-							double aVal2 = Double.parseDouble(a2.getText());
-							double bVal2 = Double.parseDouble(b2.getText());
-							double cVal2 = Double.parseDouble(c2.getText());
-							double dVal2 = Double.parseDouble(d2.getText());
-							q2 = new Quaternion(aVal2, bVal2, cVal2, dVal2);
-
-							output = "q1 + q2 = \r\n\r\n" + q1.add(q2);
-							JOptionPane.showMessageDialog(null, output);
-						} catch (Exception e) {
-							JOptionPane.showMessageDialog(null, "Please enter two valid quaternions.");
-						}
-					} else if (subtract.isSelected()) {
-						try {
-							double aVal1 = Double.parseDouble(a1.getText());
-							double bVal1 = Double.parseDouble(b1.getText());
-							double cVal1 = Double.parseDouble(c1.getText());
-							double dVal1 = Double.parseDouble(d1.getText());
-							q1 = new Quaternion(aVal1, bVal1, cVal1, dVal1);
-
-							double aVal2 = Double.parseDouble(a2.getText());
-							double bVal2 = Double.parseDouble(b2.getText());
-							double cVal2 = Double.parseDouble(c2.getText());
-							double dVal2 = Double.parseDouble(d2.getText());
-							q2 = new Quaternion(aVal2, bVal2, cVal2, dVal2);
-
-							output = "q1 - q2 = \r\n\r\n" + q1.subtract(q2);
-							JOptionPane.showMessageDialog(null, output);
-						} catch (Exception e) {
-							JOptionPane.showMessageDialog(null, "Please enter two valid quaternions.");
-						}
-					} else if (multiply.isSelected()) {
-						try {
-							double aVal1 = Double.parseDouble(a1.getText());
-							double bVal1 = Double.parseDouble(b1.getText());
-							double cVal1 = Double.parseDouble(c1.getText());
-							double dVal1 = Double.parseDouble(d1.getText());
-							q1 = new Quaternion(aVal1, bVal1, cVal1, dVal1);
-
-							double aVal2 = Double.parseDouble(a2.getText());
-							double bVal2 = Double.parseDouble(b2.getText());
-							double cVal2 = Double.parseDouble(c2.getText());
-							double dVal2 = Double.parseDouble(d2.getText());
-							q2 = new Quaternion(aVal2, bVal2, cVal2, dVal2);
-
-							output = "q1 * q2 = \r\n\r\n" + q1.multiply(q2);
-							JOptionPane.showMessageDialog(null, output);
-						} catch (Exception e) {
-							JOptionPane.showMessageDialog(null, "Please enter two valid quaternions.");
-						}
-					} else if (divide.isSelected()) {
-						try {
-							double aVal1 = Double.parseDouble(a1.getText());
-							double bVal1 = Double.parseDouble(b1.getText());
-							double cVal1 = Double.parseDouble(c1.getText());
-							double dVal1 = Double.parseDouble(d1.getText());
-							q1 = new Quaternion(aVal1, bVal1, cVal1, dVal1);
-
-							double aVal2 = Double.parseDouble(a2.getText());
-							double bVal2 = Double.parseDouble(b2.getText());
-							double cVal2 = Double.parseDouble(c2.getText());
-							double dVal2 = Double.parseDouble(d2.getText());
-							q2 = new Quaternion(aVal2, bVal2, cVal2, dVal2);
-
-							output = "q1 / q2 = \r\n\r\n" + q1.divide(q2);
-							JOptionPane.showMessageDialog(null, output);
-						} catch (Exception e) {
-							JOptionPane.showMessageDialog(null, "Please enter two valid quaternions.");
-						}
-					} else {
-						JOptionPane.showMessageDialog(null, "Please select a mathematical operation.");
-					}
-				} else {
-					try {
-						double aVal = Double.parseDouble(a1.getText());
-						double bVal = Double.parseDouble(b1.getText());
-						double cVal = Double.parseDouble(c1.getText());
-						double dVal = Double.parseDouble(d1.getText());
-						q1 = new Quaternion(aVal, bVal, cVal, dVal);
-
-						output = "Conjugate: \r\n" + q1.getConjugate() + "\r\n\r\nInverse: \r\n" + q1.getInverseFraction()
-								+ "\r\n" + q1.getInverse() + "\r\n\r\nNorm: \r\n" + q1.getNormRadical()
-								+ "\r\n" + q1.getNorm();
-						JOptionPane.showMessageDialog(null, output);
-					} catch (Exception e) {
-						JOptionPane.showMessageDialog(null, "Please enter a valid quaternion");
-					}
-				}
-			}
-		});
+		calc.addActionListener(new ButtonHandler());
 		calc.setBounds(10, 232, 89, 23);
 		frame.getContentPane().add(calc);
 
 		frame.setVisible(true);
+	}
+
+	private class ButtonHandler implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			// Detect whether the user has inputted two quaternions
+			try {
+				double z = Double.parseDouble(a2.getText()) + Double.parseDouble(b2.getText())
+						+ Double.parseDouble(c2.getText()) + Double.parseDouble(d2.getText());
+				twoInputs = true;
+			} catch (Exception e) {
+				twoInputs = false;
+			}
+
+			if (twoInputs) {
+				if (add.isSelected()) {
+					try {
+						double aVal1 = Double.parseDouble(a1.getText());
+						double bVal1 = Double.parseDouble(b1.getText());
+						double cVal1 = Double.parseDouble(c1.getText());
+						double dVal1 = Double.parseDouble(d1.getText());
+						q1 = new Quaternion(aVal1, bVal1, cVal1, dVal1);
+
+						double aVal2 = Double.parseDouble(a2.getText());
+						double bVal2 = Double.parseDouble(b2.getText());
+						double cVal2 = Double.parseDouble(c2.getText());
+						double dVal2 = Double.parseDouble(d2.getText());
+						q2 = new Quaternion(aVal2, bVal2, cVal2, dVal2);
+
+						output = "q1 + q2 = \r\n\r\n" + q1.add(q2);
+						JOptionPane.showMessageDialog(null, output);
+					} catch (Exception e) {
+						JOptionPane.showMessageDialog(null, "Please enter two valid quaternions.");
+					}
+				} else if (subtract.isSelected()) {
+					try {
+						double aVal1 = Double.parseDouble(a1.getText());
+						double bVal1 = Double.parseDouble(b1.getText());
+						double cVal1 = Double.parseDouble(c1.getText());
+						double dVal1 = Double.parseDouble(d1.getText());
+						q1 = new Quaternion(aVal1, bVal1, cVal1, dVal1);
+
+						double aVal2 = Double.parseDouble(a2.getText());
+						double bVal2 = Double.parseDouble(b2.getText());
+						double cVal2 = Double.parseDouble(c2.getText());
+						double dVal2 = Double.parseDouble(d2.getText());
+						q2 = new Quaternion(aVal2, bVal2, cVal2, dVal2);
+
+						output = "q1 - q2 = \r\n\r\n" + q1.subtract(q2);
+						JOptionPane.showMessageDialog(null, output);
+					} catch (Exception e) {
+						JOptionPane.showMessageDialog(null, "Please enter two valid quaternions.");
+					}
+				} else if (multiply.isSelected()) {
+					try {
+						double aVal1 = Double.parseDouble(a1.getText());
+						double bVal1 = Double.parseDouble(b1.getText());
+						double cVal1 = Double.parseDouble(c1.getText());
+						double dVal1 = Double.parseDouble(d1.getText());
+						q1 = new Quaternion(aVal1, bVal1, cVal1, dVal1);
+
+						double aVal2 = Double.parseDouble(a2.getText());
+						double bVal2 = Double.parseDouble(b2.getText());
+						double cVal2 = Double.parseDouble(c2.getText());
+						double dVal2 = Double.parseDouble(d2.getText());
+						q2 = new Quaternion(aVal2, bVal2, cVal2, dVal2);
+
+						output = "q1 * q2 = \r\n\r\n" + q1.multiply(q2);
+						JOptionPane.showMessageDialog(null, output);
+					} catch (Exception e) {
+						JOptionPane.showMessageDialog(null, "Please enter two valid quaternions.");
+					}
+				} else if (divide.isSelected()) {
+					try {
+						double aVal1 = Double.parseDouble(a1.getText());
+						double bVal1 = Double.parseDouble(b1.getText());
+						double cVal1 = Double.parseDouble(c1.getText());
+						double dVal1 = Double.parseDouble(d1.getText());
+						q1 = new Quaternion(aVal1, bVal1, cVal1, dVal1);
+
+						double aVal2 = Double.parseDouble(a2.getText());
+						double bVal2 = Double.parseDouble(b2.getText());
+						double cVal2 = Double.parseDouble(c2.getText());
+						double dVal2 = Double.parseDouble(d2.getText());
+						q2 = new Quaternion(aVal2, bVal2, cVal2, dVal2);
+
+						output = "q1 / q2 = \r\n\r\n" + q1.divide(q2);
+						JOptionPane.showMessageDialog(null, output);
+					} catch (Exception e) {
+						JOptionPane.showMessageDialog(null, "Please enter two valid quaternions.");
+					}
+				} else {
+					JOptionPane.showMessageDialog(null, "Please select a mathematical operation.");
+				}
+			} else {
+				try {
+					double aVal = Double.parseDouble(a1.getText());
+					double bVal = Double.parseDouble(b1.getText());
+					double cVal = Double.parseDouble(c1.getText());
+					double dVal = Double.parseDouble(d1.getText());
+					q1 = new Quaternion(aVal, bVal, cVal, dVal);
+
+					output = "Conjugate: \r\n" + q1.getConjugate() + "\r\n\r\nInverse: \r\n" + q1.getInverseFraction()
+							+ "\r\n" + q1.getInverse() + "\r\n\r\nNorm: \r\n" + q1.getNormRadical() + "\r\n"
+							+ q1.getNorm();
+					JOptionPane.showMessageDialog(null, output);
+				} catch (Exception e) {
+					JOptionPane.showMessageDialog(null, "Please enter a valid quaternion.");
+				}
+			}
+		}
 	}
 
 }
