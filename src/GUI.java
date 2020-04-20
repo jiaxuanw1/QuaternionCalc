0fc70ddc0ec0cfe4720ec8cf4323647af0b19408
Jiaxuan Wang <jiaxuanw1@gmail.com>
2020-04-19 22:50:26 -0400

    Remove unused import
    Also added screenshot of interface

--- src/GUI.java
+++ src/GUI.java
@@ -7,7 +7,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
 
